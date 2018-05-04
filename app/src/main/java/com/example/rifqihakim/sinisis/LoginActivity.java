b8b8ce89311ef17df5b3989d81da431a223b6f4f
OS0418MIN\HueCd.Com <sylvia.rezhi1@gmail.com>
2018-05-04 16:04:33 +0700

    no message

--- app/src/main/java/com/example/rifqihakim/sinisis/LoginActivity.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/LoginActivity.java
@@ -2,12 +2,27 @@ package com.example.rifqihakim.sinisis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button login;
+    private EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (Button)findViewById(R.id.btnLogin);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+
     }
 }

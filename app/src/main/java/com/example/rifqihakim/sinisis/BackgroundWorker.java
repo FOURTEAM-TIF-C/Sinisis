0444a47cd530a9fe7298c0008796e315ffcd6752
OS0418MIN\HueCd.Com <sylvia.rezhi1@gmail.com>
2018-05-05 11:33:29 +0700

    login belum

--- app/src/main/java/com/example/rifqihakim/sinisis/BackgroundWorker.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/BackgroundWorker.java
@@ -1,4 +1,31 @@
 package com.example.rifqihakim.sinisis;
 
-public class BackgroundWorker {
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class BackgroundWorker extends AsyncTask<String, Void, Void> {
+    Context context;
+    BackgroundWorker (Context ctx) {
+        context = ctx;
+    }
+    @Override
+    protected Void doInBackground(String... params) {
+        String type = params [0];
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
 }
--- app/src/main/java/com/example/rifqihakim/sinisis/LoginActivity.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/LoginActivity.java
@@ -7,22 +7,23 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button login;
+
     private EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        login = (Button)findViewById(R.id.btnLogin);
         username = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
-        login.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view){
-
+        String usernameA = username.getText().toString();
+        String pass = password.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker (this);
+        backgroundWorker.execute(type, usernameA, pass);
     }
 }

92989447eda7a0479c64dafc9ddd96d27eee5eb6
RifqiHakim <rifqihakim99@gmail.com>
2018-05-07 21:53:53 +0700

    Intent Nilai

--- app/src/main/java/com/example/rifqihakim/sinisis/NilaiActivity.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/NilaiActivity.java
@@ -1,7 +1,10 @@
 package com.example.rifqihakim.sinisis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NilaiActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class NilaiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nilai);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent il= new Intent(getApplicationContext(),RaportActivity.class);
+                startActivity(il);
+            }
+        });
+
     }
 }

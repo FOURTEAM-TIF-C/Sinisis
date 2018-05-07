a1960a5b656f148a6da0839618d543d53906d06a
RifqiHakim <rifqihakim99@gmail.com>
2018-05-07 21:42:20 +0700

    intent

--- app/src/main/java/com/example/rifqihakim/sinisis/DataActivity.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/DataActivity.java
@@ -1,7 +1,10 @@
 package com.example.rifqihakim.sinisis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DataActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class DataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+
+        Button button34 = (Button) findViewById(R.id.button34);
+
+        button34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent il= new Intent(getApplicationContext(),NilaiActivity.class);
+                startActivity(il);
+            }
+        });
+        Button button22 = (Button) findViewById(R.id.button22);
+
+        button22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent il= new Intent(getApplicationContext(),PresensiActivity.class);
+                startActivity(il);
+            }
+        });
+
+
     }
 }

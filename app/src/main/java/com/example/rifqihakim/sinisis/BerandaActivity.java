b7f8c2d7abbecac5acb9308c1a4d40cf6eb84a21
RifqiHakim <rifqihakim99@gmail.com>
2018-05-08 07:44:16 +0700

    no message

--- app/src/main/java/com/example/rifqihakim/sinisis/BerandaActivity.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/BerandaActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -23,9 +24,9 @@ public class BerandaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_beranda);
 
         @SuppressLint("WrongViewCast")
-        Button imageButton = (Button) findViewById(R.id.imageButton);
+        ImageButton ImageButton = (ImageButton) findViewById(R.id.imageButton);
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        ImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent data = new Intent(getApplicationContext(), DataActivity.class);
@@ -34,9 +35,9 @@ public class BerandaActivity extends AppCompatActivity {
         });
 
         @SuppressLint("WrongViewCast")
-        Button imageButton2 = (Button) findViewById(R.id.imageButton2);
+        ImageButton ImageButton2 = (ImageButton) findViewById(R.id.imageButton2);
 
-        imageButton2.setOnClickListener(new View.OnClickListener() {
+        ImageButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent absensi = new Intent(getApplicationContext(), PresensiActivity.class);
@@ -45,9 +46,9 @@ public class BerandaActivity extends AppCompatActivity {
         });
 
         @SuppressLint("WrongViewCast")
-        Button imageButton4 = (Button) findViewById(R.id.imageButton4);
+        ImageButton ImageButton4 = (ImageButton) findViewById(R.id.imageButton4);
 
-        imageButton4.setOnClickListener(new View.OnClickListener() {
+        ImageButton4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent nilai = new Intent(getApplicationContext(), NilaiActivity.class);

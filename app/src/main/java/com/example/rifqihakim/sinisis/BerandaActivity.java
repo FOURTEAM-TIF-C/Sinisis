a48ff2fdd2fef7e929407313d60e3609dfce6f52
Ramadhanty-PC\Ramadhanty <dhanty12@gmail.com>
2018-05-08 11:42:45 +0700

    no message

--- app/src/main/java/com/example/rifqihakim/sinisis/BerandaActivity.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/BerandaActivity.java
@@ -3,6 +3,7 @@ package com.example.rifqihakim.sinisis;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
+import android.support.design.widget.NavigationView;
 import android.support.annotation.RequiresApi;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,44 +17,97 @@ import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class BerandaActivity extends AppCompatActivity {
-    
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
+    //Mendefinisikan variabel
+    private android.support.v7.widget.Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
 
-        @SuppressLint("WrongViewCast")
+        // Menginisiasi Toolbar dan mensetting sebagai actionbar
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Menginisiasi  NavigationView
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        //Mengatur Navigasi View Item yang akan dipanggil untuk menangani item klik menu navigasi
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            // This method will trigger on item Click of navigation menu
+
         ImageButton ImageButton = (ImageButton) findViewById(R.id.imageButton);
-
-        ImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent data = new Intent(getApplicationContext(), DataActivity.class);
-                startActivity(data);
-            }
-        });
-
-        @SuppressLint("WrongViewCast")
         ImageButton ImageButton2 = (ImageButton) findViewById(R.id.imageButton2);
-
-        ImageButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent absensi = new Intent(getApplicationContext(), PresensiActivity.class);
-                startActivity(absensi);
-            }
-        });
-
-        @SuppressLint("WrongViewCast")
         ImageButton ImageButton4 = (ImageButton) findViewById(R.id.imageButton4);
 
-        ImageButton4.setOnClickListener(new View.OnClickListener() {
+//        ImageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent data = new Intent(getApplicationContext(), DataActivity.class);
+//                startActivity(data);
+//            }
+//        });
+//        ImageButton2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent absensi = new Intent(getApplicationContext(), PresensiActivity.class);
+//                startActivity(absensi);
+//            }
+//        });
+//
+//        ImageButton4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent nilai = new Intent(getApplicationContext(), NilaiActivity.class);
+//                startActivity(nilai);
+//            }
+//        });
             @Override
-            public void onClick(View view) {
-                Intent nilai = new Intent(getApplicationContext(), NilaiActivity.class);
-                startActivity(nilai);
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                //Memeriksa apakah item tersebut dalam keadaan dicek  atau tidak,
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+                //Menutup  drawer item klik
+                drawerLayout.closeDrawers();
+                //Memeriksa untuk melihat item yang akan dilklik dan melalukan aksi
+                switch (menuItem.getItemId()){
+                    // pilihan menu item navigasi akan menampilkan pesan toast klik kalian bisa menggantinya
+                    //dengan intent activity
+                    case R.id.navigation1:
+                        Toast.makeText(getApplicationContext(), "Pengaturan Telah Dipilih", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation2:
+                        Toast.makeText(getApplicationContext(),"Tentang Telah Dipilih",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation3:
+                        Toast.makeText(getApplicationContext(),"Keluar Telah Dipilih",Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Kesalahan Terjadi ",Toast.LENGTH_SHORT).show();
+                        return true;
+                }
             }
         });
+        // Menginisasi Drawer Layout dan ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Kode di sini akan merespons setelah drawer menutup disini kita biarkan kosong
+                super.onDrawerClosed(drawerView);
+            }
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                //  Kode di sini akan merespons setelah drawer terbuka disini kita biarkan kosong
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        //Mensetting actionbarToggle untuk drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        //memanggil synstate
+        actionBarDrawerToggle.syncState();
     }
 }
--- app/src/main/java/com/example/rifqihakim/sinisis/LoginActivity.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/LoginActivity.java
@@ -1,20 +1,21 @@
 package com.example.rifqihakim.sinisis;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
 
 public class LoginActivity extends AppCompatActivity  {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button button11 = (Button) findViewById(R.id.btnLogin);
 
+        Button button11 = (Button) findViewById(R.id.btnLogin);
         button11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

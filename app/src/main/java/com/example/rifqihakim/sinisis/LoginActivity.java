76340c2085d52ca57d89e164f36a6ab82ccc8fcc
OS0418MIN\HueCd.Com <sylvia.rezhi1@gmail.com>
2018-05-08 08:35:59 +0700

    no message

--- app/src/main/java/com/example/rifqihakim/sinisis/LoginActivity.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.rifqihakim.sinisis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends AppCompatActivity  {
@@ -10,6 +13,15 @@ public class LoginActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button button11 = (Button) findViewById(R.id.btnLogin);
+
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent il= new Intent(getApplicationContext(),BerandaActivity.class);
+                startActivity(il);
+            }
+        });
 
     }
 

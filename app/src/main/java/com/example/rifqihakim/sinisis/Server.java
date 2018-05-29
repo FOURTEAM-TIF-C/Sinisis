a03f6eed15471011cb5a4651753413711b402807
RifqiHakim <rifqihakim99@gmail.com>
2018-05-29 21:24:12 +0700

    online

--- app/src/main/java/com/example/rifqihakim/sinisis/Server.java
+++ app/src/main/java/com/example/rifqihakim/sinisis/Server.java
@@ -1,5 +1,5 @@
 package com.example.rifqihakim.sinisis;
 
 public class Server {
-    public static final String URL = "http://192.168.43.138/android/";
+    public static final String URL = "http://sinisis.tk/android/";
 }

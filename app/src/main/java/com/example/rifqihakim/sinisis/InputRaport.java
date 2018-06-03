d2180f70af6882c36e043c36fe03e943863da20c
RifqiHakim <rifqihakim99@gmail.com>
2018-06-03 22:42:44 +0700

    input id raport & fix berberapa

---
+++ app/src/main/java/com/example/rifqihakim/sinisis/InputRaport.java
@@ -0,0 +1,85 @@
+package com.example.rifqihakim.sinisis;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+public class InputRaport extends AppCompatActivity implements View.OnClickListener{
+
+    //Dibawah ini merupakan perintah untuk mendefinikan View
+    private EditText txt_idRaport;
+    private EditText editTextDesg;
+    private EditText editTextSal;
+
+    private Button buttonAdd;
+    private Button buttonView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_raport);
+
+        //Inisialisasi dari View
+        txt_idRaport = (EditText) findViewById(R.id.txt_idRaport);
+
+
+        buttonAdd = (Button) findViewById(R.id.btn_simpan);
+
+        //Setting listeners to button
+        buttonAdd.setOnClickListener(this);
+    }
+
+
+    //Dibawah ini merupakan perintah untuk Menambahkan Pegawai (CREATE)
+    private void addEmployee(){
+
+        final String idr = txt_idRaport.getText().toString().trim();
+
+        class AddEmployee extends AsyncTask<Void,Void,String>{
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(InputRaport.this,"Menambahkan...","Tunggu...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(InputRaport.this,s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String,String> params = new HashMap<>();
+                params.put(konfigurasi.KEY_EMP_IDR,idr);
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(konfigurasi.URL_ADD_ID, params);
+                return res;
+            }
+        }
+
+        AddEmployee ae = new AddEmployee();
+        ae.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonAdd){
+            addEmployee();
+        }
+
+    }
+}
